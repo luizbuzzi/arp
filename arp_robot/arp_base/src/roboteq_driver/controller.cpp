ca5202ee0c64157c1bc241b97b236fe3a3734ea3
Luiz Henrique Buzzi <luizbuzzi@gmail.com>
2017-09-06 15:27:21 -0300

    testes no hardware e ajustes no algoritmo

--- arp_robot/arp_base/src/roboteq_driver/controller.cpp
+++ arp_robot/arp_base/src/roboteq_driver/controller.cpp
@@ -113,7 +113,7 @@ void Controller::connect()
 
 void Controller::read()
 {
-  serial_->flush();
+  serial_->flushInput();
   ROS_DEBUG_STREAM_NAMED("serial", "Bytes waiting: " << serial_->available());
   std::string msg = serial_->readline(max_line_length, eol);
   if (!msg.empty())
