be200549be49c1a5d36dc3b121a6907d2c839450
arp <base@base.arp>
2017-09-25 10:47:00 -0300

    Atualização dda leitura do controlador

--- arp_robot/arp_base/src/roboteq_driver/controller.cpp
+++ arp_robot/arp_base/src/roboteq_driver/controller.cpp
@@ -114,8 +114,7 @@ void Controller::connect()
 void Controller::read()
 {
   ROS_DEBUG_STREAM_NAMED("serial", "Bytes waiting: " << serial_->available());
-  std::string msg;
-  std::vector<std::string> msg_t = serial_->readlines(max_line_length, eol);
+  std::string msg = serial_->readline(max_line_length, eol);
 
   if (!msg.empty())
   {
